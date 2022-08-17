e5f45091a84051d213782c71935e660110c83c36
divBaral <076bct068.saroj@pcampus.edu.np>
2022-08-17 22:31:06 +0545

    Bounding box implemented

--- Project/src/Renderer.cpp
+++ Project/src/Renderer.cpp
@@ -71,150 +71,88 @@ sf::Color applyLighting2(Vector light, Vector normal, Vector view, float ambient
 
 	return(sf::Color(color[0], color[1], color[2]));
 }
+//distance of point Q from line containing points A & B
+float lineDistance2D(const Point2d& A,const Point2d& B, const Point2d& Q){
+
+	Vector2d n(A.y - B.y, B.x - A.x);
+	const float d = A.x*B.y - B.x*A.y;
+	return (n.dot(Q)+d)/n.length();
+}
+
+float bary2D(const Point2d& A, const Point2d& B, const Point2d& C, const Point2d& Q){
+	return ((lineDistance2D(B,C,Q))/(lineDistance2D(B,C,A)));
+}
 void Renderer::DrawTriangle(Point &point0, Point &point1, Point &point2, Matrix4f &viewspace, sf::Image &image, material& material)
 {
     // converting to 2d coordinates for viewport transform
+	// viewspace = Matrix4f();
     auto q0 = viewspace * point0;
     auto q1 = viewspace * point1;
     auto q2 = viewspace * point2;
     Point2d p0 = {q0.x, q0.y};
     Point2d p1 = {q1.x, q1.y};
     Point2d p2 = {q2.x, q2.y};
+	viewport(p0, p1, p2);
+    //compute bb
+    auto x0 = std::min({p0.x, p1.x, p2.x});
+    auto y0 = std::min({p0.y, p1.y, p2.y});
+    auto x1 = std::max({p0.x, p1.x, p2.x});
+    auto y1 = std::max({p0.y, p1.y, p2.y});
 
-    // viewport transform
-    
-    viewport(p0, p1, p2);
+	float w = m_window->getSize().x;
+    float h = m_window->getSize().y;
 
-    // here p0,p1,p2 are coordinates scaled to screen size
-    // and point0,point1,point2 are original coordinates i.e. not scaled to screen size
+	if(x0<0){x0=0;}
+	if(y0<0){y0=0;}
+	if(x1>w){x1=w;}
+	if(y1>h){y1=h;}	
 
-    // temporary clipping jugaad
-    int minx = std::min({p0.x, p1.x, p2.x});
-    int maxx = std::max({p0.x, p1.x, p2.x});
-    int miny = std::min({p0.y, p1.y, p2.y});
-    int maxy = std::max({p0.y, p1.y, p2.y});
+	Vector vertexPw[3] = {q0, q1, q2};
+	float vertexW[3] = {-1/q0.z, -1/q1.z, -1/q2.z};
+	Vector n = {1,1,1};//normals to imported.
+	Vector vertexNw[3] = {n,n, n};
+   
+    for ( int y=y0; y<y1; ++y) {
+        for ( int x = x0; x<x1; ++x) {
+			
+            Point2d Q(x+0.5f, y+0.5f);
+            float alpha = bary2D(p0, p1, p2, Q);
+            float beta = bary2D(p2, p0, p1, Q);
+            float gamma = bary2D(p1, p2, p0, Q);
+            float weight2D[] = {alpha, beta, gamma};
+            if(weight2D[0]>0 && weight2D[1]>0 && weight2D[2]>0){
+					//interpolate depth
+					float w = 0.0f;
+					for(int k = 0; k<3; ++k){
+						w += weight2D[k] * vertexW[k];
+					}
+					Point pW = Point();
+					Vector nW = Vector();
+					//interpolate projective attributes
+					for(int k = 0;k<3;++k){
+						pW += vertexPw[k]*weight2D[k];
+						nW += vertexNw[k]*weight2D[k];
+					}
 
-    if (maxx < m_window->getSize().x && maxy < m_window->getSize().y && minx >= 0 && miny >= 0)
-    {
-        using SlopeData = std::pair<float, float>;
+					//recover interpolated attributes
+					const Point& P = pW/w;
+					const Vector& n = nW/w;
 
-        // unpacking the coordinates of the triangle
-        auto x0 = p0.x, y0 = p0.y;
-        auto x1 = p1.x, y1 = p1.y;
-        auto x2 = p2.x, y2 = p2.y;
+					const float depth = P.length();
 
-        // sorting three coordinates, so that
-        // the topmost coordinate is assigned to (X0,Y0)
-        // the middle corner is assigned to (X1,Y1)
-        // the bottom corner is assigned to (X2,Y2)
-        // ordering first by Y-coordinate and then by X-coordinate
-        if (std::tie(y1, x1) < std::tie(y0, x0))
-        {
-            std::swap(x0, x1);
-            std::swap(y0, y1);
-            std::swap(p0, p1);
+					//depth test
+					if( m_zBuffer->testAndSet( x, y, depth)){
+						auto color = sf::Color::White;
+						if (image.getSize().x != 0 || image.getSize().y != 0)
+                        	color = image.getPixel((x / m_window->getSize().x) * image.getSize().x, (y / m_window->getSize().y) * image.getSize().y);
+						
+						sf::Vertex v(sf::Vector2f(x, y), color);
+                    	m_window->draw(&v, 1, sf::Points);
+						
+					}
+				}
 
-            // swapping unscaled/original coordinates
-            std::swap(point0, point1);
-            std::swap(q0, q1);
         }
-
-        if (std::tie(y2, x2) < std::tie(y0, x0))
-        {
-            std::swap(x0, x2);
-            std::swap(y0, y2);
-            std::swap(p0, p2);
-
-            // swapping unscaled/original coordinates
-            std::swap(point0, point2);
-            std::swap(q0, q2);
-        }
-
-        if (std::tie(y2, x2) < std::tie(y1, x1))
-        {
-            std::swap(x1, x2);
-            std::swap(y1, y2);
-            std::swap(p1, p2);
-
-            // swapping unscaled/original coordinates
-            std::swap(point1, point2);
-             std::swap(q1, q2);
-        }
-
-        // If triangle has no area, return
-        if (y0 == y2)
-            return;
-
-        // calculating the direction ratios of normal to the surface of the triangle p0,p1,p2
-        Vector side1(p2.x - p1.x, p2.y - p1.y, q2.z - q1.z);
-        Vector side2(p0.x - p1.x, p0.y - p1.y, q0.z - q1.z);
-        // //cross product
-        Vector normal = side1.cross(side2);
-
-        // checking if the bend in the triangle lies on either right side or on left side
-        bool shortSide = (y1 - y0) * (x2 - x0) < (x1 - x0) * (y2 - y0);
-        // false: short side is in left side
-        // true: short side is in right side
-
-        // if the bend is in the right side flip the normal
-        if (shortSide)
-            normal = normal * (-1);
-        normal = normal.normalize();
-
-        // calculating direction cosines of the normal to the triangle
-        float a = normal.x;
-        float b = normal.y;
-        float c = normal.z;
-        // calculating constant value in the equation of the triangle
-        float d = -1 * (a * q0.x + b * q0.y + c * q0.z);
-
-        // filling the triangle
-        RasterizeTriangle(
-            p0, p1, p2,
-            [&](const auto &p)
-            {
-                std::pair<float, float> q = {p.x, p.y};
-                return q;
-            },
-            [&](const auto &p0, const auto &p1, float step)
-            {
-                int begin = p0.x;
-                int end = p1.x;
-
-                return SlopeData(begin, (end - begin) / step);
-            },
-            [&](int y, SlopeData &left, SlopeData &right, auto &&Plot)
-            {
-                int x = left.first;
-                int endx = right.first;
-
-                for (; x <= endx; ++x)
-                {
-                    float depth = (-a *x - b * y - d) / c;
-                    Plot(x, y, depth);
-                }
-
-                left.first += left.second;
-                right.first += right.second;
-            },
-            [&](int x, int y, float depth)
-            {
-                if (m_zBuffer->testAndSet(x, y, depth))
-                {
-                    sf::Color color = sf::Color(0, 255, 0);
-                    if (image.getSize().x != 0 || image.getSize().y != 0)
-                    color = image.getPixel((x / m_window->getSize().x) * image.getSize().x, (y / m_window->getSize().y) * image.getSize().y);
-
-                    Vector vertexcoordinates = 
-                    Vector light = {4, 4, 4};
-                    Vector campos = {0,0,0};
-                    // normal
-                    //
-                    //material.print();
-                    sf::Color cf = applyLighting2(light-vertexcoordinates , normal, campos-vertexcoordinates,0.4, material );
-                    sf::Vertex v(sf::Vector2f(x, y), cf);
-                    m_window->draw(&v, 1, sf::Points);
-                }
-            });
     }
+   
 }
